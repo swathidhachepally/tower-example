c2057a1edcf048328badb378215c205d58e8d035
swathidhachepally <40208563+swathidhachepally@users.noreply.github.com>
2024-02-04 08:50:13 -0600

    Update example.py

--- example.py
+++ example.py
@@ -1,2 +1,8 @@
-# example.py
-print("Hello from Python script!")
+# create_file.py
+file_content = "This is the content of the file."
+
+with open("example_file.txt", "w") as file:
+    file.write(file_content)
+
+print("File 'example_file.txt' created with contents:", file_content)
+
